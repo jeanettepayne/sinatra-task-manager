67fe10440a6d376b46ce65960f60864eaa3ffe05
jeanettepayne <jeanetterpayne@gmail.com>
2020-11-23 11:42:35 -0500

    added helper methods in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,16 @@ class ApplicationController < Sinatra::Base
         set :session_secret, "password_security"
     end
 
+    helpers do
+
+        def logged_in?
+            !!current_user
+        end
+
+        def current_user
+            @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+        end
+
+    end
+
 end
--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,3 +1,3 @@
 class TaskController < ApplicationController
-    
+
 end
