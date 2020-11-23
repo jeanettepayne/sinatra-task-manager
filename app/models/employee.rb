c8da2280f61a4797d794839920f75ca135a53803
jeanettepayne <jeanetterpayne@gmail.com>
2020-11-23 07:35:23 -0500

    added slugifiable methods to manager and employee classes

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,12 @@ class Employee < ActiveRecord::Base
     belongs_to :manager
     has_many :tasks
     has_secure_password
+
+    def slug
+        self.name.gsub(" ", "-").downcase
+    end
+
+    def self.find_by_slug(slug)
+        self.all.find{|name| name.slug == slug}
+    end
 end
--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,4 +1,13 @@
 class Manager < ActiveRecord::Base
     has_many :employees
     has_secure_password
+
+    def slug
+        self.name.gsub(" ", "-").downcase
+    end
+
+    def self.find_by_slug(slug)
+        self.all.find{|name| name.slug == slug}
+    end
+    
 end
