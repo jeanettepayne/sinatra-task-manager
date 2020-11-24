c006b5bcfdc4586aa2ba1b89359c2dd777838ce8
jeanettepayne <jeanetterpayne@gmail.com>
2020-11-23 19:31:47 -0500

    added /tasks/new get and post routes, added create_task form

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,9 +1,18 @@
 class TaskController < ApplicationController
 
+    get '/tasks/new' do
+        erb :'tasks_views/create_task'
+    end
+
+    post '/tasks' do
+        
+    end
+    
     get '/tasks/:employee_slug' do
         @employee = Employee.find_by_slug(params[:employee_slug])
 
         erb :'tasks_views/show_tasks'
     end
+
 
 end
