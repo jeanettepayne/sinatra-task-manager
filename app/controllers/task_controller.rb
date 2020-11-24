aee8fabb2a59a909dc81efd302c1c1ffc0621449
jeanettepayne <jeanetterpayne@gmail.com>
2020-11-23 19:35:56 -0500

    changed route for new task creation, added header to create_task erb page

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,17 +1,27 @@
 class TaskController < ApplicationController
 
-    get '/tasks/new' do
-        erb :'tasks_views/create_task'
-    end
+    # get '/tasks/:employee_slug/new' do
+    #     erb :'tasks_views/create_task'
+    # end
 
-    post '/tasks' do
-        
-    end
+    # post '/tasks' do
+
+    # end
     
     get '/tasks/:employee_slug' do
         @employee = Employee.find_by_slug(params[:employee_slug])
 
         erb :'tasks_views/show_tasks'
+    end
+
+    get '/tasks/:employee_slug/new' do
+        @employee = Employee.find_by_slug(params[:employee_slug])
+        
+        erb :'tasks_views/create_task'
+    end
+
+    post '/tasks/:employee_slug/new' do
+
     end
 
 
